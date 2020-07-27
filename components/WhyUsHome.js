eff221ad666661c99ffc5c5f3ca066484873d273
Arnav Narula <arnavnarula.77@gmail.com>
2020-07-26 23:50:24 -0700

    changed some home page text that gave me ocd bc of spacing

--- components/WhyUsHome.js
+++ components/WhyUsHome.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from "./text.module.css";
-import {Button, Card, CardBody, CardTitle, Col, Row} from "shards-react";
+import { Button, Card, CardBody, CardTitle, Col, Row } from "shards-react";
 
 export default class WhyusHome extends React.Component {
     render() {
@@ -17,8 +17,8 @@ export default class WhyusHome extends React.Component {
                                 <Col md={3}>
                                     <Card style={{ maxWidth: "300px", background: "rgba(0,0,0,0)" }} className="m-0 p-0">
                                         <CardBody className="m-0 p-4">
-                                            <CardTitle className={styles.sectionTitle}>APCSA</CardTitle>
-                                            <p className={styles.sectionSubtitle}>This course will familiarize students with the content of a regular APCSA curriculum with no prior Java experience required. Students will learn about the basics of Java syntax and typical content of an APCSA class including variables, Objects, common loops, methods, and basic data structures like arrays.</p>
+                                            <CardTitle className={styles.sectionTitle}>Intro to Java</CardTitle>
+                                            <p className={styles.sectionSubtitle}>This course will familiarize students with the content of a regular APCSA curriculum with no prior Java experience required. Students will learn about the basics of Java syntax and typical content of an APCSA class including variables, Objects, and basic data structures like arrays while working on numerous, fun coding projects along the way.</p>
                                             <Button href="/classes#introtojava">View course &rarr;</Button>
                                         </CardBody>
                                     </Card>
@@ -26,8 +26,8 @@ export default class WhyusHome extends React.Component {
                                 <Col md={3}>
                                     <Card style={{ maxWidth: "300px", background: "rgba(0,0,0,0)" }} className="m-0 p-0">
                                         <CardBody className="m-0 p-4">
-                                            <CardTitle className={styles.sectionTitle}>Data Structures</CardTitle>
-                                            <p className={styles.sectionSubtitle}>Advanced Data Structures (ADSs) will familiarize students with beyond HS level CS concepts to supplement their knowledge from APCSA. Students will be introduced to ADSs like Hash Tables and Linked Lists, learn about their properties and implementation, and solve real world problems using these data structures.</p>
+                                            <CardTitle className={styles.sectionTitle}>Advanced Data Structures</CardTitle>
+                                            <p className={styles.sectionSubtitle}>Advanced Data Structures (ADSs) will familiarize students with beyond HS level CS concepts to supplement their knowledge from APCSA. Students will be introduced to ADSs like Hash Tables, learn about their properties and implementation, and solve real world problems using these data structures.</p>
                                             <Button href="/classes#datastructures">View course &rarr;</Button>
                                         </CardBody>
                                     </Card>
@@ -36,7 +36,7 @@ export default class WhyusHome extends React.Component {
                                     <Card style={{ maxWidth: "300px", background: "rgba(0,0,0,0)" }} className="m-0 p-0">
                                         <CardBody className="m-0 p-4">
                                             <CardTitle className={styles.sectionTitle}>Data Science</CardTitle>
-                                            <p className={styles.sectionSubtitle}>In this course, students will be taught the process of creating a data science notebook using Jupyter. Students will gain a level of familiarity with numerous python libraries including nympty, pandas, and matplotlib and learn how to manipulate, analyze, and visualize real life data from datasets to answer a research question.</p>
+                                            <p className={styles.sectionSubtitle}>In this course, students will be taught the process of creating a data science notebook using Jupyter. Students will gain a level of familiarity with numerous python libraries including nympty, pandas, matplotlib, and more to learn how to manipulate, analyze, and visualize real life data from datasets in order to answer a single research question.</p>
                                             <Button href="/classes#datascience">View course &rarr;</Button>
                                         </CardBody>
                                     </Card>
@@ -45,7 +45,7 @@ export default class WhyusHome extends React.Component {
                                     <Card style={{ maxWidth: "300px", background: "rgba(0,0,0,0)" }} className="m-0 p-0">
                                         <CardBody className="m-0 p-4">
                                             <CardTitle className={styles.sectionTitle}>Game Development</CardTitle>
-                                            <p className={styles.sectionSubtitle}>Through Unity, students will learn the main features involved in game development to empower them to create their own 2D game. This course will cover C# syntax and methods from scratch before diving into coding player movement, animation, UI, and more to create your very own, customizable platformer game!</p>
+                                            <p className={styles.sectionSubtitle}>Through Unity, students will learn the main features involved in game development to empower them to create their own 2D game. This course will cover C# syntax and methods from scratch before diving into coding player movement, animation, UI, and more to create a customizable platformer game!</p>
                                             <Button href="/classes#gamedevelopment">View course &rarr;</Button>
                                         </CardBody>
                                     </Card>
@@ -59,7 +59,7 @@ export default class WhyusHome extends React.Component {
                     <div className="whyussection">
                         <h1 className={`${styles.sectionTitle} ${styles.leftAlign}`}>Cause-centric</h1>
                         <h4 className={`${styles.sectionSubtitle} ${styles.leftAlign} ${styles.padRight}`}>
-                            100% of the donations will go to BLM organizations and organizations supporting minority students in STEM such as NAACP, Color of Change, and National Society of Black Engineers.
+                            100% of donations will go toward BLM and organizations supporting minority students in STEM such as NAACP, Color of Change, and National Society of Black Engineers.
                         </h4>
                     </div>
                     <div className="whyussection">
